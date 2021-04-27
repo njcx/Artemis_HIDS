f071512c933375ae20d81ce49482c6e97d17e38c
njcx <njcx86@gmail.com>
2021-04-27 14:01:57 +0800

    add readme

--- utils/log/log.go
+++ utils/log/log.go
@@ -7,20 +7,19 @@ import (
 )
 
 var (
-	Info *log.Logger
+	Info    *log.Logger
 	Warning *log.Logger
-	Error * log.Logger
+	Error   *log.Logger
 )
 
-func init(){
-	errFile,err:=os.OpenFile("/var/log/p-agent.log",os.O_CREATE|os.O_WRONLY|os.O_APPEND,0666)
-	if err!=nil{
-		log.Fatalln("打开日志文件失败：",err)
+func init() {
+	errFile, err := os.OpenFile("/var/log/artemis-agent.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("打开日志文件失败：", err)
 	}
 
-	Info = log.New(io.MultiWriter(os.Stdout,errFile),"Info:",log.Ldate | log.Ltime | log.Lshortfile)
-	Warning = log.New(io.MultiWriter(os.Stdout,errFile),"Warning:",log.Ldate | log.Ltime | log.Lshortfile)
-	Error = log.New(io.MultiWriter(os.Stderr,errFile),"Error:",log.Ldate | log.Ltime | log.Lshortfile)
+	Info = log.New(io.MultiWriter(os.Stdout, errFile), "Info:", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(io.MultiWriter(os.Stdout, errFile), "Warning:", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(io.MultiWriter(os.Stderr, errFile), "Error:", log.Ldate|log.Ltime|log.Lshortfile)
 
 }
-
